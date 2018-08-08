1b6c359a2edd6fd81c4598e0acfb6966a1535262
dimireme <a_s_d@mail.ru>
2018-08-08 12:13:35 +0300

    Add styles to the App component.

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 
+import style from'./app.css';
+
 import NavContainer from './containers/NavContainer';
 import SearchContainer from './containers/SearchContainer';
 import FlightsListContainer from './containers/FlightsListContainer';
@@ -7,7 +9,7 @@ import FlightsListContainer from './containers/FlightsListContainer';
 export default class App extends PureComponent {
 	render() {
 		return (
-			<div>
+			<div className={style.container}>
 				<NavContainer/>
 				<SearchContainer/>
 				<FlightsListContainer/>
